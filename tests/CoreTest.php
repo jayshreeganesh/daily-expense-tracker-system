fa93121107452535e3e39912f491d75c5b447f32
jayshreeganesh <jayshreeganesh@users.noreply.github.com>
2026-08-15 22:52:54 +0530

    test: Add PHPUnit support and configure GitHub Actions to run automated backend testing

---
+++ tests/CoreTest.php
@@ -0,0 +1,8 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class CoreTest extends TestCase {
+    public function testTrueIsTrue() {
+        $this->assertTrue(true, "Sanity check: True is true.");
+    }
+}
